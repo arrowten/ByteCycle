1d6da226be8a174d22385394c6e1254cef687b7b
Ajay Wakodikar <ajaywakodikarsocial@gmail.com>
2025-12-10 12:32:48 +0000

    CPU class: update_zn_flags function (definition)

--- sources/cpu.cxx
+++ sources/cpu.cxx
@@ -14,3 +14,9 @@ void CPU::set_flag(std::uint8_t bit) {
 void CPU::clear_flag(std::uint8_t bit) {
     flags & ~bit;
 }
+
+/// Updates the zero and negative CPU flags based on an 8-bit result value.
+void CPU::update_zn_flags(std::uint8_t result) {
+    (result == 0) ? set_flag(FLAG_Z) : clear_flag(FLAG_Z);
+    (result & 0x80) ? set_flag(FLAG_N) : clear_flag(FLAG_N);
+}
